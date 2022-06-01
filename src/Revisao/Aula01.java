3250faaecec3d897d9af9d59f5e70f3e65f76608
vich928 <victorhs928@gmail.com>
2022-06-01 12:58:00 -0300

    commit inicial

---
+++ src/Revisao/Aula01.java
@@ -0,0 +1,12 @@
+package Revisao;
+
+import java.util.Scanner;
+
+public class Aula01 {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		for(int i=0;i<=10;++i) {
+			System.out.println(i);
+		}
+	}
+}
